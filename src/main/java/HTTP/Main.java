3659eabbb1d033e2e6a2938dbc0c4858cfcb2ed5
Al Baraa <albraa9021@gmail.com>
2025-08-26 21:45:44 +0400

    Added Socket and simple program to show today date

--- src/main/java/HTTP/Main.java
+++ src/main/java/HTTP/Main.java
@@ -1,7 +1,22 @@
 package HTTP;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Let us build HTTP Server in Java because why not");
+    public static void main(String[] args) throws Exception {
+        final ServerSocket server = new ServerSocket(8080);
+        System.out.println("Listening for connection on port 8080 ....");
+        while (true) {
+            try (Socket socket = server.accept()) {
+                Date today = new Date();
+                String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+                socket.getOutputStream()
+                        .write(httpResponse.getBytes("UTF-8"));
+            }
+        }
     }
 }
